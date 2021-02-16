c8dd0b678fbed7d1560245b8581c856f7f060ce6
Mark Kamradt <mark.kamradt@gmail.com>
2021-02-15 22:03:14 -0800

    daily update

--- byweek.R
+++ byweek.R
@@ -6,9 +6,6 @@ source("weekpoint.R")
 
 data <- read_csv("turnips.csv", skip=15)
 
-# ggthemr("lilac") -- not working ggplot color parameter
-
-
 mdata <- data %>%
      mutate(day = paste(floor_date(make_date(YEAR,MONTH,DAY), "week"),ISLAND)) %>%
      rowwise() %>%
